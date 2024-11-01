ce68b148c0bcc59783f558be83c09c56d7a1a1e9
billy <billy@gonoph.net>
2024-10-31 22:56:15 -0400

    updated triangle-obfuscated to remove compile warnings about parentheses

--- triangle-obfuscated.c
+++ triangle-obfuscated.c
@@ -7,8 +7,8 @@
 const int c=(int)0x0a202a;
 int p(int r,int i,int o)
 {pc(I(0));if(!i)pc(R);
-else pc(I(1));return(r>3
-&&!i||!(r+i+o)?0:p(i?r:
+else pc(I(1));return((r>3
+&&!i)||!(r+i+o)?0:p(i?r:
 rr(o,r),i?i-1:rr(o,r),o))
 ;}int main(void){return p
 (0,0,1)+p(3,3,0);}
